781490b00bf4d25c729a94f058a00cc70a5eb6c0
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:44:44 +0545

    Create signup.js

---
+++ src/container/signup.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Signuppage from '../component/register';
+import Header from '../component/header';
+
+
+class Signup extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <Header />
+                <Signuppage />
+
+
+
+
+            </div>
+
+        )
+    }
+
+
+}
+export default Signup
