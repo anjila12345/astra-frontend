fbb157f004be00a16cf2b1ffa7bedfb763b7ba02
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:45:09 +0545

    Create login.js

---
+++ src/container/login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Login from '../component/login';
+import Header from '../component/header';
+
+import Footer from '../component/footer';
+
+
+class Loginpage extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <Header />
+                <Login />
+                <Footer />
+
+
+
+
+            </div>
+
+        )
+    }
+
+
+}
+export default Loginpage
