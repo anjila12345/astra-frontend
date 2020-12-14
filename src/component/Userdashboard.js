48e97e185f88994aa63c646c42940d6dc6b9000c
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:45:37 +0545

    Create Userdashboard.js

---
+++ src/component/Userdashboard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+
+
+class userdashboard extends React.Component {
+
+    render() {
+
+        return (
+            <div >
+
+                <div className="content-wrapper1">
+
+                    <section id="candidates" className="content-header">
+                        <div className="container">
+                            <div className="row">
+
+                                <div className="col-md-9 bg-white padding-2">
+                                    <form action="#" method="post" enctype="multipart/form-data" className="p-5 bg-white">
+                                        <div class="container-form">
+
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        )
+    }
+}
+export default userdashboard
