792173625b29a68a73378cf2b2e5405a62de44b2
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:45:28 +0545

    Create home.js

---
+++ src/component/home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import axios from 'react'
+
+
+
+class Indexpage extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <section className="content-header bg-main">
+                            <div className="row">
+                                <div className="col-md-12 text-center index-head">
+                                    <h1 class="colortext"><span><strong>ASTRA</strong></span></h1>
+                                    <h2 class="colortext2"><span><strong>Guard</strong></span></h2>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+
+            </div>
+
+        )
+    }
+
+
+}
+export default Indexpage
