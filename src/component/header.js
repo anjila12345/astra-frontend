1750b717537dea5e1c32992352ce6fa17f43f1f8
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:45:22 +0545

    Create header.js

---
+++ src/component/header.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import axios from 'axios'
+
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            isLoggedIn: false
+        }
+
+        this.state = {
+            config: {
+                headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+            }
+        }
+
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/logincheck', this.state.config)
+            .then((response) => {
+                this.setState({ isLoggedIn: true })
+            });
+    }
+
+    // form handler
+    handleChange = (e) => {
+        this.setState(
+            { [e.target.name]: e.target.value }
+        )
+    }
+    //login function  handler 
+    submitForm = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3000/login', this.state)
+            .then((response) => {
+                localStorage.setItem('token', response.data.token)
+                this.setState({ isLoggedIn: true })
+            }).catch((err) => console.log(err.response))
+        this.setState({ email: '', password: '' })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <header class="main-header">
+
+                    <a href="/" class="logo  ">
+                        <span class="logo-lg"><h1><b>Astra</b><b>Securities</b></h1></span>
+                    </a>
+
+
+                    <nav class="navbar navbar-static-top">
+
+                        <div class="navbar-custom-menu">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="Login"><div className="btnhome">Login</div></a>
+                                </li>
+                                <li>
+                                    <a href="register"><div className="btnhome1">Signup</div></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+
+        )
+    }
+
+
+}
+export default Header
