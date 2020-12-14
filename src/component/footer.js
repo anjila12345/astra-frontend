cde7fc7dfc3e58e7e8bf930bc087ffc5b5b230ba
anjila12345 <anjilasubedi95@gmail.com>
2020-12-14 21:45:16 +0545

    Create footer.js

---
+++ src/component/footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+class Footer extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+
+                <footer className="main-footer" style={{ marginleft: 0 }}>
+                    <div className="text-center" style={{ fontWeight: 700 }}>
+                        Copyright &copy; 2020 | Astra Securities
+       	<div>
+
+                        </div>
+                    </div>
+                </footer>
+
+            </div>
+
+        )
+    }
+
+
+}
+export default Footer
