678a571fabda425356389a0e06067578d03454b3
anjila12345 <anjilasubedi95@gmail.com>
2020-12-15 22:04:31 +0545

    Update header.js

--- src/component/Users/header.js
+++ src/component/Users/header.js
@@ -1,46 +1,42 @@
 import React from 'react'
 import axios from 'axios'
 
-
 class Header extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            email: '',
-            password: '',
             isLoggedIn: false
         }
-
         this.state = {
+            id: '',
+            singleFeed: {},
+            post_status: '',
+            profileimage: '',
+            name: '',
+            post: [],
+            user: {},
             config: {
                 headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
             }
         }
-
     }
 
     componentDidMount() {
         axios.get('http://localhost:3000/logincheck', this.state.config)
             .then((response) => {
-                this.setState({ isLoggedIn: true })
+                this.setState({
+                    isLoggedIn: true,
+                    user: response.data
+                })
             });
-    }
 
-    // form handler
-    handleChange = (e) => {
-        this.setState(
-            { [e.target.name]: e.target.value }
-        )
+
+
+
     }
-    //login function  handler 
-    submitForm = (e) => {
-        e.preventDefault();
-        axios.post('http://localhost:3000/login', this.state)
-            .then((response) => {
-                localStorage.setItem('token', response.data.token)
-                this.setState({ isLoggedIn: true })
-            }).catch((err) => console.log(err.response))
-        this.setState({ email: '', password: '' })
+    LogOut = () => {
+        axios.post('http://localhost:3000/userlogout')
+        localStorage.removeItem('token')
     }
 
     render() {
@@ -61,16 +57,21 @@ class Header extends React.Component {
                                 <a href="Newsfeed">
                                     <div class="title"><i class="fa fa-home"></i>Home</div></a>
 
-                                <a href="blog">
-                                    <div class="title"><i class="	fa fa-plus"></i>Post</div></a>
+                                <a href="Editprofile">
+                                    <div class="title"><i class="	fa fa-plus"></i>Profile</div></a>
 
                                 <a href="Mypost">
                                     <div class="title"><i class="	fa fa-newspaper-o"></i>User post
                 </div></a>
 
+                                <a href="Editprofile">
+                                    <div class="title">
+
+                                        {this.state.user.firstname}
+                                    </div></a>
 
 
-
+                                <a href="Login" onClick={this.LogOut}>  <i class="fa fa-sign-out">Logout</i> </a>
 
                             </div>
                         </div>
