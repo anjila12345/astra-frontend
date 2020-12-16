6926f8faeea1ea168ee0a4fd0d30093e26d322ba
anjila12345 <anjilasubedi95@gmail.com>
2020-12-16 09:36:52 +0545

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,12 @@ import Loginpage from './container/login'
 import Home from './container/home'
 import Signup from './container/signup'
 import userdash from './container/userdashboard'
+import Editprofile from '././container/Users/editprofile'
 
+
+// import Login from './container/Agent/login'
+
+// import Register from './container/Agent/register'
 
 class Router extends Component {
     render() {
@@ -17,10 +22,11 @@ class Router extends Component {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Loginpage} />
-
                     <Route exact path="/register" component={Signup} />
-
+                    {/* <Route exact path="/agentlogin" component={Login} />
+                    <Route exact path="/agentregister" component={Register} /> */}
                     <Route exact path="/Userdashboard" component={userdash} />
+                    <Route exact path="/Editprofile" component={Editprofile} />
                 </Switch>
             </BrowserRouter>
         )
