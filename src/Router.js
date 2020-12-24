495cc7df8e132a76fcb33cf16683430406c04ac3
anjila12345 <anjilasubedi95@gmail.com>
2020-12-24 17:40:01 +0545

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -9,11 +9,15 @@ import Home from './container/home'
 import Signup from './container/signup'
 import userdash from './container/userdashboard'
 import Editprofile from '././container/Users/editprofile'
+import Newsfeed from '././container/Users/Newsfeed'
 
-
-// import Login from './container/Agent/login'
-
-// import Register from './container/Agent/register'
+// Agent
+import Login from './container/Agent/login'
+import Agentregister from './container/Agent/register'
+import Register2 from './component/register2'
+import Agentprofile from './container/Agent/editprofile'
+import Postjob from './container/Agent/addjob'
+import Mypost from './container/Agent/mypost'
 
 class Router extends Component {
     render() {
@@ -23,10 +27,15 @@ class Router extends Component {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Loginpage} />
                     <Route exact path="/register" component={Signup} />
-                    {/* <Route exact path="/agentlogin" component={Login} />
-                    <Route exact path="/agentregister" component={Register} /> */}
+                    <Route exact path="/agentlogin" component={Login} />
+                    <Route exact path="/signup" component={Register2} />
+                    <Route exact path="/agentregister" component={Agentregister} />
                     <Route exact path="/Userdashboard" component={userdash} />
                     <Route exact path="/Editprofile" component={Editprofile} />
+                    <Route exact path="/agentprofile" component={Agentprofile} />
+                    <Route exact path="/postjob" component={Postjob} />
+                    <Route exact path="/mypost" component={Mypost} />
+                    <Route exact path="/Newsfeed" component={Newsfeed} />
                 </Switch>
             </BrowserRouter>
         )
