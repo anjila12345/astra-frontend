a318df364f7683a6f6ed2ea530860f5c488245bf
samita21 <48872843+samita21@users.noreply.github.com>
2021-01-18 13:55:00 +0545

    testing for resgister.
    input fields testing.

--- src/Test/userregister.test.js
+++ src/Test/userregister.test.js
@@ -23,6 +23,22 @@ describe('Login component tests', () => {
     expect(wrapper.find('h2').text()).toContain("Register Here");
   });
 
-  
+  it('should have input for email and password', () => {
+    //Email and password input field should be present
+    expect(wrapper.find('input#firstname')).toHaveLength(1);
+    expect(wrapper.find('input#lastname')).toHaveLength(1);
+    expect(wrapper.find('input#email')).toHaveLength(1);
+    expect(wrapper.find('input#username')).toHaveLength(1);
+    expect(wrapper.find('input#password')).toHaveLength(1);
+  });
+
+  it('should have an empty email and password state var', () => {
+    //Optionally test to check if password and email are empty strings on 
+    expect(wrapper.state('firstname')).toEqual('');
+    expect(wrapper.state('lastname')).toEqual('');
+    expect(wrapper.state('email')).toEqual('');
+    expect(wrapper.state('username')).toEqual('');
+    expect(wrapper.state('password')).toEqual('');
+  });
 
 });
