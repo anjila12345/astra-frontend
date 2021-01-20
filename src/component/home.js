377233cbc007ff2781e3aae1e43fae282dad198c
anjila12345 <anjilasubedi95@gmail.com>
2021-01-20 12:30:49 +0545

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -65,8 +65,6 @@ class Indexpage extends React.Component {
                                             </ul>
                                         </div>
                                     </div>
-<<<<<<< Updated upstream
-=======
                                 </div>
 
                             </div>
@@ -525,133 +523,13 @@ class Indexpage extends React.Component {
                                         </div>
                                     </div>
 
->>>>>>> Stashed changes
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-<<<<<<< Updated upstream
-                    <div id="services" class="services-area area-padding">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                    <div class="section-headline services-head text-center">
-                                        <h2>Our Services</h2>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row text-center">
-
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <div class="about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-gear"></i>
-                                                </a>
-                                                <h4>Profile Management</h4>
-                                                <p>
-                                                    The client can register, login, create & manage his/her profile.
-                  </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <div class="about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-camera-retro"></i>
-                                                </a>
-                                                <h4>Job Posting</h4>
-                                                <p>
-                                                    The client can post job/ jobs as per the need, the customer can specify the job type.
-                  </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-
-                                    <div class=" about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-users"></i>
-                                                </a>
-                                                <h4>Hire</h4>
-                                                <p>
-                                                    The client can hire a contractor or guard for a specific period of time.
-                  </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-
-                                    <div class=" about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-search"></i>
-                                                </a>
-                                                <h4>Search for job </h4>
-                                                <p>
-                                                    The client can search for a suitable contractor or agency as per the need. </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-
-                                    <div class=" about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-user-plus"></i>
-                                                </a>
-                                                <h4>Follow</h4>
-                                                <p>
-                                                    The client can follow agent account for detail information about job.
-                  </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-
-                                    <div class=" about-move">
-                                        <div class="services-details">
-                                            <div class="single-services">
-                                                <a class="services-icon" href="#">
-                                                    <i class="fa fa-star"></i>
-                                                </a>
-                                                <h4>Rating & Reviews</h4>
-                                                <p>
-                                                    After completion of the job, the client can drop his feedback.  </p>
-                                            </div>
-                                        </div>
-
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-=======
 
->>>>>>> Stashed changes
                 </main>
             </div>
-
         )
     }
 
